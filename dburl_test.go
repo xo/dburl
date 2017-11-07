d7b4de8194d8dc19b5803b80d4d3409d7e5c09dd
Kenneth Shaw <kenneth.shaw@knq.io>
2017-11-07 09:19:08 +0700

    Minor change

--- dburl_test.go
+++ dburl_test.go
@@ -108,7 +108,7 @@ func TestParse(t *testing.T) {
 
 		{`oracle://user:pass@localhost/xe.oracle.docker`, `ora`, `user/pass@localhost/xe.oracle.docker`}, // 41
 
-		{`presto://host:8001/`, `presto`, `http://user@host:8001?catalog=default`},
+		{`presto://host:8001/`, `presto`, `http://user@host:8001?catalog=default`}, // 42
 		{`presto://host/catalogname/schemaname`, `presto`, `http://user@host:8080?catalog=catalogname&schema=schemaname`},
 		{`prs://admin@host/catalogname`, `presto`, `https://admin@host:8443?catalog=catalogname`},
 		{`prestodbs://admin:pass@host:9998/catalogname`, `presto`, `https://admin:pass@host:9998?catalog=catalogname`},
